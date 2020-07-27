e435b2e3d684cc84e130f5d468ce1162c1f635e2
Paolo Cozzi <bunop@libero.it>
2020-07-27 17:42:14 +0200

    :sparkles: crteate home component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatTabsModule
   ],
   exports: [
-
+    MatTabsModule
   ],
   declarations: []
 })
