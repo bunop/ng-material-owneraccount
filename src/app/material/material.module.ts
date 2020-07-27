63ebefecbea64e541ab74fea8608b266a61bd2d8
Paolo Cozzi <bunop@libero.it>
2020-07-27 18:03:17 +0200

    :sparkles: start working on navigation

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatSidenavModule
   ],
   exports: [
-    MatTabsModule
+    MatTabsModule,
+    MatSidenavModule
   ],
   declarations: []
 })
